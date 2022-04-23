189e3c5948ecf4a12c9581d9e08e2bfe0363bbab
laurent <laurent.mazare@gmail.com>
2022-04-22 21:19:12 -0400

    Add the custom type.

--- src/custom.rs
+++ src/custom.rs
@@ -43,3 +43,41 @@ pub fn get<'a, T: 'static>(v: crate::Value<'a, Box<T>>) -> &'a UnsafeCell<T> {
     let v = unsafe { *ocaml_sys::field(v.value, 1) as *mut Box<dyn std::any::Any> };
     unsafe { &**v }.downcast_ref::<UnsafeCell<T>>().expect("unexpected box content")
 }
+
+use std::sync::{Arc, Mutex};
+
+/// A wrapped Rust value protected by a mutex and seen as an abstract type
+/// from OCaml.
+pub struct Custom<T> {
+    _inner: Arc<Mutex<T>>,
+}
+
+impl<T: 'static> crate::to_value::ToValue for Custom<T> {
+    fn to_value<F, U>(&self, pin: F) -> U
+    where
+        U: Sized,
+        F: FnOnce(ocaml_sys::Value) -> U,
+    {
+        let box_: Box<Box<dyn std::any::Any>> = Box::new(Box::new(self._inner.clone()));
+        let boxed_t = Box::into_raw(box_);
+        let sys_value = unsafe {
+            ocaml_sys::caml_alloc_custom(
+                &CUSTOM_OPERATIONS_FOR_BOX,
+                std::mem::size_of::<Box<Box<dyn std::any::Any>>>(),
+                0,
+                1,
+            )
+        };
+        let ptr = unsafe { ocaml_sys::field(sys_value, 1) } as *mut _;
+        unsafe { std::ptr::write(ptr, boxed_t) };
+        pin(sys_value)
+    }
+}
+
+impl<T: 'static> crate::from_value::FromSysValue for Custom<T> {
+    unsafe fn from_value(v: ocaml_sys::Value) -> Self {
+        let v = *ocaml_sys::field(v, 1) as *mut Box<dyn std::any::Any>;
+        let inner = { &**v }.downcast_ref::<Arc<Mutex<T>>>().expect("unexpected box content");
+        Custom { _inner: inner.clone() }
+    }
+}
