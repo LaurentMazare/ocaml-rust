ddee7f9d946b561aeb272c68d664dd201fdfbc9d
laurent <laurent.mazare@gmail.com>
2022-05-01 10:41:31 -0400

    More arrow testing.

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ test:
 	cargo build --manifest-path ./example/Cargo.toml
 	cargo run --manifest-path ./gen/cmd/Cargo.toml -- --rust-file ./example/src/lib.rs --ocaml-file ./tests/ocaml/test_gen.ml
 	cp ./target/debug/libocaml_rust_example.a tests/ocaml/
+	cargo run --manifest-path ./gen/cmd/Cargo.toml -- --rust-file ./example-arrow/src/lib.rs --ocaml-file ./tests/ocaml/arrow_gen.ml
+	cp ./target/debug/libocaml_rust_arrow.a tests/ocaml/
 	dune runtest --root=tests/ocaml --force --no-buffer
 	cargo test
 
--- tests/ocaml/arrow_test.ml
+++ tests/ocaml/arrow_test.ml
@@ -6,36 +6,38 @@ let ok_exn = function
   | Ok ok -> ok
   | Error err -> Printf.failwithf "%s" err ()
 
-let%expect_test _ =
-  let array_len = 10 in
-  let rb =
-    let array_foo =
-      Array.init array_len ~f:(fun i -> Float.of_int i |> Float.sqrt)
-      |> Arrow.array_f64_from
-    in
-    let array_foo_ba =
-      Array.init array_len ~f:(fun i -> 1. /. (1. +. Float.of_int i))
-      |> Bigarray.Array1.of_array Float64 C_layout
-      |> Arrow.array_f64_from_ba
-    in
-    let array_bar =
-      Array.init array_len ~f:(Printf.sprintf "b<%d>") |> Arrow.array_string_from
-    in
-    Arrow.record_batch_create
-      [| "foo", array_foo; "foo_ba", array_foo_ba; "bar", array_bar |]
-    |> ok_exn
+let test_record_batch ~array_len =
+  let array_foo =
+    Array.init array_len ~f:(fun i -> Float.of_int i |> Float.sqrt)
+    |> Arrow.array_f64_from
   in
+  let array_foo_ba =
+    Array.init array_len ~f:(fun i -> 1. /. (1. +. Float.of_int i))
+    |> Bigarray.Array1.of_array Float64 C_layout
+    |> Arrow.array_f64_from_ba
+  in
+  let array_bar =
+    Array.init array_len ~f:(Printf.sprintf "b<%d>") |> Arrow.array_string_from
+  in
+  Arrow.record_batch_create
+    [| "foo", array_foo; "foo_ba", array_foo_ba; "bar", array_bar |]
+  |> ok_exn
+
+let%expect_test _ =
+  let rb = test_record_batch ~array_len:10 in
   Stdio.printf
     "%s\n%!"
     (Arrow.record_batch_schema rb |> [%sexp_of: Arrow.schema] |> Sexp.to_string_hum);
-  [%expect {|
+  [%expect
+    {|
     ((fields
       (((name foo) (data_type Float64) (nullable false))
        ((name foo_ba) (data_type Float64) (nullable false))
        ((name bar) (data_type Utf8) (nullable false))))
      (metadata ())) |}];
   Stdio.printf "%s\n%!" (Arrow.record_batch_debug rb);
-  [%expect {|
+  [%expect
+    {|
     RecordBatch { schema: Schema { fields: [Field { name: "foo", data_type: Float64, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: None }, Field { name: "foo_ba", data_type: Float64, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: None }, Field { name: "bar", data_type: Utf8, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: None }], metadata: {} }, columns: [PrimitiveArray<Float64>
     [
       0.0,
@@ -72,4 +74,30 @@ let%expect_test _ =
       "b<7>",
       "b<8>",
       "b<9>",
-    ]], row_count: 10 } |}]
+    ]], row_count: 10 } |}];
+  let () =
+    try
+      let _column = Arrow.record_batch_column rb 123 in
+      Stdio.printf "SHOULD HAVE FAILED!\n"
+    with
+    | Failure str -> Stdio.printf "failed as expected, %s\n%!" (String.prefix str 68)
+  in
+  [%expect
+    {| failed as expected, panicked at 'index out of bounds: the len is 3 but the index is 123' |}];
+  let column = Arrow.record_batch_column rb 0 |> Arrow.array_i64_values in
+  Stdio.printf "%s\n%!" ([%sexp_of: Int64.t array option] column |> Sexp.to_string_mach);
+  [%expect {| () |}];
+  let column = Arrow.record_batch_column rb 0 |> Arrow.array_f64_values in
+  Stdio.printf "%s\n%!" ([%sexp_of: float array option] column |> Sexp.to_string_mach);
+  let column = Arrow.record_batch_column rb 1 |> Arrow.array_f64_values in
+  Stdio.printf "%s\n%!" ([%sexp_of: float array option] column |> Sexp.to_string_mach);
+  let column = Arrow.record_batch_column rb 2 |> Arrow.array_string_values in
+  Stdio.printf
+    "%s\n%!"
+    ([%sexp_of: string option array option] column |> Sexp.to_string_mach);
+  [%expect
+    {|
+    ((0 1 1.4142135623730951 1.7320508075688772 2 2.23606797749979 2.4494897427831779 2.6457513110645907 2.8284271247461903 3))
+    ((1 0.5 0.33333333333333331 0.25 0.2 0.16666666666666666 0.14285714285714285 0.125 0.1111111111111111 0.1))
+    (((b<0>)(b<1>)(b<2>)(b<3>)(b<4>)(b<5>)(b<6>)(b<7>)(b<8>)(b<9>))) |}];
+  [%expect {||}]
